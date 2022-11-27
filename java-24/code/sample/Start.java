a66ef9966d83f0cb88ad5e0158f2d7bca4428cea
excaribur <phonyiam.jirayut@gmail.com>
2022-11-28 00:17:57 +0700

    update pom

--- java-24/code/sample/Start.java
+++ java-24/code/sample/Start.java
@@ -1,5 +1,8 @@
 package sample;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,45 +12,60 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 class Start {
-    public static void main(String[] data){
-        ApplicationContext context = SpringApplication.run(Setup.class);
+	public static void main(String[] data) {
+		ApplicationContext context = SpringApplication.run(Setup.class);
+	}
+}
+@RestController
+class Sample {
+	@RequestMapping("/list-branch")
+	Iterable listBranch() {
+		return repository.findAll();
+	}
+	@RequestMapping("/list-product")
+	Iterable listProduct() {
+		return product.findAll();
+	}
+	@Autowired BranchRepository repository;
+	@Autowired ProductRepository product;
+}
 
-    }
+@Repository
+interface ProductRepository extends CrudRepository<Product, Long> { }
+
+@Table(name="product")
+class Product {
+	@Id public long number;
+	public String name;
+	public String size;
+	public double price;
+}
+
+@Component
+interface BranchRepository extends CrudRepository<Branch, Integer> { }
+
+@Table(name="branch")
+class Branch {
+	@Id public int number;
+	public String name;
+	public double area;
 }
 
 @SpringBootApplication
 class Setup {
-    @Bean
-    DriverManagerDataSource connect() {
-        return new DriverManagerDataSource(connectionString);
-    }
-    String connectionString = "jdbc:mysql://localhost/" + "company?user=jeff&password=bezos";       
-}
-
-@RestController
-class Sample {
-    @RequestMapping("/list-product")
-    Iterable listProduct() {
-        return product.findAll();
-    }
-    
-    @Autowired ProductRepository product;
-}
-
-@Repository    
-interface ProductRepository extends CrudRepository<Product, Long> {}
-
-@Table(name = "product")
-class Product {
-    @Id public long number;
-    public String name;
-    public String size;
-    public double price;
+	@Bean
+	DriverManagerDataSource connect() {
+		return new DriverManagerDataSource(connectionString);
+	}
+	String connectionString = "jdbc:mysql://localhost/" +
+			"company?user=jeff&password=bezos";	
 }
 
 
